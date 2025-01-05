c254815fd334dea3f0e3089249f7930441f1844f
Florian Thiel <flth@flth.de>
2025-01-05 21:07:16 +0100

    setze Texte auf Übersetzen

--- mod.lua
+++ mod.lua
@@ -11,35 +11,35 @@ return {
 				key = "bue_signal_at",
 				name = _("Österreich"),
 				uiType = "CHECKBOX",
-				values = { "No", "Yes", },				
+				values = { _("no"), _("yes"), },
 				defaultIndex = 0,
 			},
 		},
 		authors = {
 			{
 				name = "ModWerkstatt",
-				role = "CREATOR",		
-			}, 
+				role = "CREATOR",
+			},
 		},
 		tags = { "Signal", "Bahnübergang", "crossing signal", },
 		dependencies = { "modwerkstatt_signalkomponenten_1" },
 	},
     runFn = function (settings, modParams)
-		
-		local function ATFilter(fileName, data)		
-			if data.metadata.signal and data.metadata.bue_signale and data.metadata.bue_signale.country == "at" then			
+
+		local function ATFilter(fileName, data)
+			if data.metadata.signal and data.metadata.bue_signale and data.metadata.bue_signale.country == "at" then
 				print "yes"
                 data.metadata.availability.yearFrom = 1
 				data.metadata.availability.yearTo = 2
 				--return false
-			end	
+			end
 			--return true
 			return data
-		end 
+		end
 
         if modParams[getCurrentModId()] ~= nil then
-			local params = modParams[getCurrentModId()]					
-			if params["bue_signal_at"] == 0 then				
+			local params = modParams[getCurrentModId()]
+			if params["bue_signal_at"] == 0 then
 				addModifier("loadModel", ATFilter)
 			end
         end
